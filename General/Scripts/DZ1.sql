493790235b504ef08d4bfb6468b3e987a52f144e
qod8bop <turcenkovtimofej99228@gmail.com>
2025-12-02 21:38:56 +0300

    nnnnnnnnn

---
+++ General/Scripts/DZ1.sql
@@ -0,0 +1,47 @@
+--Без связей
+create table if not exists Genre (
+	id SERIAL primary key, 
+	name VARCHAR(60) not NULL
+);
+
+create table if not exists Performer (
+	id SERIAL primary key, 
+	name VARCHAR(60) not NULL
+);
+
+create table if not exists Albums (
+	id SERIAL primary key,
+	name VARCHAR(80) not null,
+	year VARCHAR(5)
+);
+
+create table if not exists Sborniki(
+	id SERIAL primary key,
+	name VARCHAR(60) not NULL
+);
+
+
+--Один к многим
+create table if not exists Tracklist(
+	id SERIAL primary key,
+	album_id INTEGER references Albums(id),
+	trackname VARCHAR(60),
+	length time not NULL
+);
+
+
+--Многие к многим
+create table if not exists pergen (
+	Per_id INTEGER references Performer(id),
+	Gen_id INTEGER references Genre(id)
+);
+
+create table if not exists peralb (
+	Per_id INTEGER REFERENCES Performer(id),
+	Alb_id INTEGER references Albums(id)
+);
+
+create table if not exists SBTR (
+	SB_id INTEGER references Sborniki(id),
+	TR_id INTEGER references Tracklist(id)
+);
